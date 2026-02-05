6eaec74f8c04902d8838e706dcaf79fb35947d8b
Alexey Shevchenko <o.shevchenko@relay42.com>
2026-02-05 23:12:54 +0100

    fix duplicates

--- install_macos.sh
+++ install_macos.sh
@@ -2,5 +2,5 @@
 
 GOARCH=arm64 GOOS=darwin go build -o ./launchers/TradingViewRefresh.app/Contents/MacOS/tradingview-refresh
 
-cp -r ./js/ ./launchers/TradingViewRefresh.app/js/
 cp -r ./launchers/TradingViewRefresh.app/ /Applications/TradingViewRefresh.app/
+cp -r ./js/ /Applications/TradingViewRefresh.app/Contents/MacOS/js/
